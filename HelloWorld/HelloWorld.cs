aa2c1df5bbfdb954e54489896dab6ec7239e7199
Wes Garbee <wgarbee@gmail.com>
2018-10-01 22:30:50 -0500

    Updated Hello World to reflect questions CS program

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -4,7 +4,7 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Random(string[] args)
         {
             Console.WriteLine("Hello, Wes!");
         }
