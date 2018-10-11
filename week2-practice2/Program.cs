6bae9ac11737dd78fe9c6095bee2c6b7e7b12530
Wes Garbee <wgarbee@gmail.com>
2018-10-10 19:55:18 -0500

    fixed title of folder and files

---
+++ week2-practice2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace week2_practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Enter your grade: [must be an integer]");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num >= 90)
+            {
+                Console.WriteLine("You made an A");
+            }
+            else if (num >= 80)
+            {
+                Console.WriteLine("You made an B");
+            }
+            else if (num >= 70)
+            {
+                Console.WriteLine("You made an C");
+            }
+            else
+            {
+                Console.WriteLine("You did not pass");
+            }
+        }
+    }
+}
