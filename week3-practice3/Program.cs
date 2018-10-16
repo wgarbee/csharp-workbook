2da68884b374a6f1f67a6d73107cc360b5db47c8
Wes Garbee <wgarbee@gmail.com>
2018-10-15 20:06:46 -0500

    added week 3 practice files

---
+++ week3-practice3/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace week3_practice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool allTestsPassed = tests();
+
+            if (allTestsPassed)
+            {
+                Console.WriteLine("Your tests passed!");
+            }
+            else
+            {
+                Console.WriteLine("Your tests failed!");
+            }
+
+            int num1;
+            int num2;
+            int newNum = 0;
+
+            Console.WriteLine("Enter an integer: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter another integer: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            newNum = AddNumbers(num1, num2);
+
+            Console.WriteLine("Your sum is " + newNum);
+
+            
+        }
+
+        public static int AddNumbers(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static bool tests()
+        {
+            if (!test1())
+            {
+                return false;
+            }
+            if (!test2())
+            {
+                return false;
+            }
+            if (!test3())
+            {
+                return false;
+            }
+            if (!test4())
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool test1()
+        {
+            return 10 == AddNumbers(3, 7);
+        }
+
+        public static bool test2()
+        {
+            return 18 == AddNumbers(21, -3);
+        }
+
+        public static bool test3()
+        {
+            return -1 == AddNumbers(-5, 4);
+        }
+
+        public static bool test4()
+        {
+            return -24 == AddNumbers(-10, -14);
+        }
+    }
+}
---
+++ week3-practice4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace week3_practice4
+{
+    class Program
+    {
+        // static void Main(string[] args)
+        // {
+        //     String[] names = new String[4];
+
+        //     names[0] = "James";
+        //     names[1] = "Mike";
+        //     names[2] = "Adam";
+        //     names[3] = "Jane";
+
+        //     String[] pets = {"Rocky", "Walter", "Bailey"};
+
+        //     String[] cities;
+        //     cities = new String[] {"Austin", "Dallas", "Houston"};
+
+        //     Console.WriteLine("Names: {0}, {1}, {2}, {3}.", names);
+        //     Console.WriteLine("Pets: {0}, {1}, {2}.", pets);
+        //     Console.WriteLine("Cities: {0}, {1}, {2}.", cities);
+
+        //     String last = names[names.Length-1];
+
+        //     Console.WriteLine("Last name in the names array is {0}.", last);
+        // }
+
+        static void Main(string[] args)
+        {
+            List<String> names = new List<String>();
+
+            names.Add("John");
+            names.Add("Mike");
+            names.Add("Mark");
+
+            Console.WriteLine("The list contains {0} elements.", names.Count);
+            Console.WriteLine("The first element in the list is {0}.", names[0]);
+        }
+    }
+}
