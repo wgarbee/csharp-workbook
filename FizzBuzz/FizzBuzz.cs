52ecee9368894be6f1157ce83783ff21339b91cf
Wes Garbee <wgarbee@gmail.com>
2018-10-17 22:43:13 -0500

    updating fizzbuzz and w3p5

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -4,9 +4,143 @@ namespace FizzBuzz
 {
     class Program
     {
+        public static int userInput;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int loop;
+            do
+            {
+                Console.WriteLine("Please enter an integer: ");
+                userInput = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What loop do you want to use? Enter 1, 2, or 3. Type 0 to quit");
+                Console.WriteLine("1. For Loop");
+                Console.WriteLine("2. While Loop");
+                Console.WriteLine("3. Do While Loop");
+                Console.WriteLine("4. Run all three loop options");
+                loop = Convert.ToInt32(Console.ReadLine());
+
+                WhichLoop(loop);
+            } while(loop != 0);
+        }
+
+        public static void WhichLoop(int loop)
+        {
+            if (loop == 1)
+            {
+                UseForLoop();
+            }
+            else if (loop == 2)
+            {
+                UseWhileLoop();
+            }
+            else if (loop == 3)
+            {
+                UseDoWhileLoop();
+            }
+            else if (loop == 4)
+            {
+                UseForLoop();
+                UseWhileLoop();
+                UseDoWhileLoop();
+            }
+        }
+
+        // Runs a 'for loop' to find all numbers divisible by three, five, or both three and five
+        // and changes to 'fizz' - three (3), 'buzz' - five (5), or three and five - 'fizz buzz'
+        public static void UseForLoop()
+        {
+            String output = "";
+            for (int i = 1; i <= userInput; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    output = output + "fizz buzz, ";
+                }
+                else if (i % 3 == 0)
+                {
+                    output = output + "fizz, ";
+                }
+                else if (i % 5 == 0)
+                {
+                    output = output + "buzz, ";
+                }
+                else
+                {
+                    output = output + Convert.ToString(i) + ", ";
+                }
+            }
+
+            Console.WriteLine(output);
+
+            return;
+        }
+
+        // Runs a 'while loop' to find all numbers divisible by three, five, or both three and five
+        // and changes to 'fizz' - three (3), 'buzz' - five (5), or three and five - 'fizz buzz'
+        public static void UseWhileLoop()
+        {
+            String output = "";
+            int i = 1;
+
+            while (i <= userInput)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    output = output + "fizz buzz, ";
+                }
+                else if (i % 3 == 0)
+                {
+                    output = output + "fizz, ";
+                }
+                else if (i % 5 == 0)
+                {
+                    output = output + "buzz, ";
+                }
+                else
+                {
+                    output = output + Convert.ToString(i) + ", ";
+                }
+
+                i++;
+            }
+
+            Console.WriteLine(output);
+
+            return;
+        }
+
+        // Runs a 'do while loop' to find all numbers divisible by three, five, or both three and five
+        // and changes to 'fizz' - three (3), 'buzz' - five (5), or three and five - 'fizz buzz'
+        public static void UseDoWhileLoop()
+        {
+            String output = "";
+            int i = 1;
+
+            do
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    output = output + "fizz buzz, ";
+                }
+                else if (i % 3 == 0)
+                {
+                    output = output + "fizz, ";
+                }
+                else if (i % 5 == 0)
+                {
+                    output = output + "buzz, ";
+                }
+                else
+                {
+                    output = output + Convert.ToString(i) + ", ";
+                }
+
+                i++;
+            } while (i <= userInput);
+
+            Console.WriteLine(output);
+
+            return;
         }
     }
 }
---
+++ week3-practice5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace week3_practice5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = {9, 13, 4, 12, 3};
+            int comp1 = 0;
+            int comp2 = 0;
+            int num = 0;
+            int i = 0;
+
+            while (i < x.Length)
+            {
+                comp1 = x[i];
+                comp2 = x[i+1];
+
+                if (comp1 >= comp2)
+                {
+                    num = comp1;
+                }
+
+                i++;
+            }
+
+            Console.WriteLine("Largest number is {0}", num);
+
+        }
+    }
+}
