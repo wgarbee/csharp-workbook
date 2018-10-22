afab83f6159b113e0adc4321a2eecd6808ef4bfb
Wes Garbee <wgarbee@gmail.com>
2018-10-22 11:21:33 -0500

    updated and corrected the '==Y' if for first letter rule. Add tests

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -10,7 +10,13 @@ namespace PigLatin
             String word = "";
             
             // Clears the screen to begin
-            Console.Clear(); 
+            Console.Clear();
+
+            if(tests()){
+                Console.WriteLine("Tests passed.");
+            } else {
+                Console.WriteLine("Tests failed.");
+            }
             
             Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             Console.WriteLine("Elcomeway otay ethay igpay atinlay anslatortray!");
@@ -37,7 +43,7 @@ namespace PigLatin
             Console.Clear();
         }
         
-        public static string TranslateWord(string word)
+        /*  public static string TranslateWord(string word)
         {
             char[] vowel = {'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'};
             int firstVowelLocation;
@@ -73,6 +79,60 @@ namespace PigLatin
             }
 
             return word;
+        }   */
+
+        public static string TranslateWord(string word)
+        {
+            char[] vowel = {'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'};
+            int firstVowelLocation;
+            String firstVowel;
+
+            // Parses the word and locates the first vowel. If one does not exist, returns -1
+            firstVowelLocation = word.IndexOfAny(vowel);
+
+            // If the value is -1, meaning no vowels, then add yay to end of the word
+            if (firstVowelLocation < 0)
+            {
+                word += "ay";
+            }  // If the vowel is the first letter...
+            else if (firstVowelLocation == 0)
+            {
+                // assigns the vowel to variable
+                firstVowel = word.Substring(firstVowelLocation,1);
+
+                // If it's not a Y, run this and check if it is not a 
+                if (firstVowel.ToUpper() != "Y")
+                {
+                    word += "yay";
+                } // If the first letter is a Y, reinitiallize the vowel array without Y char
+                else if (firstVowel.ToUpper() == "Y")
+                {
+                    vowel = new char[] {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+                    
+                    // Find the first non-Y vowel and store to firstVowelLoaction
+                    firstVowelLocation = word.IndexOfAny(vowel);
+                    // firstVowel = word.Substring(firstVowelLocation,1);
+
+                    word = word.Substring(firstVowelLocation) + word.Substring(0, firstVowelLocation) + "ay";
+                }
+            }   // Takes the value from first vowel on, adds everything before the vowel, and adds ay
+            else
+            {
+                word = word.Substring(firstVowelLocation) + word.Substring(0, firstVowelLocation) + "ay";
+            }
+
+            return word;
+        }
+
+        public static bool tests(){
+            return 
+                TranslateWord("elephant") == "elephantyay" &&
+                TranslateWord("fox") == "oxfay" &&
+                TranslateWord("choice") == "oicechay" && 
+                TranslateWord("dye") == "yeday" && 
+                TranslateWord("bystander") == "ystanderbay" &&
+                TranslateWord("yellow") == "ellowyay" &&
+                TranslateWord("tsktsk") == "tsktskay";
         }
     }
 }
