e9e21ca51fe8cac4c4c9a8e75e30ac7227c40d13
Wes Garbee <wgarbee@gmail.com>
2018-10-24 20:26:22 -0500

    adding Gradebook proj:

---
+++ Gradebook/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentEntry();
+        }
+
+        public static void StudentEntry()
+        {
+            Dictionary<String, int> student = new Dictionary<string, int>();
+            Stack<String> name = new Stack<String>();
+            int grade;
+            String userInput;
+
+            Console.WriteLine("Please enter the students first name: ");
+            name = Console.ReadLine();
+            
+            do
+            {
+                Console.WriteLine("Please enter the students grade. Type Q to quit.");
+                grade = Convert.ToInt32(Console.ReadLine());
+            }while (userInput != "Q");
+        }
+    }
+}
