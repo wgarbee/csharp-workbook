e3af8fe767321d961754b565e6729e2979a9a721
Wes Garbee <wgarbee@gmail.com>
2018-11-05 19:30:42 -0600

    build Start Wars

---
+++ StarWars/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Person leia = new Person("Leia", "Organa", "Rebel");
+		Person darth = new Person("Darth", "Vader", "Imperial");
+		Ship falcon = new Ship("Rebel", "Smuggling", 2);
+		Ship tie = new Ship("Tie", "Fighter", 1);
+        // Station rebelSpaceStation = new Station("Yavin 4");
+        // Station deathStar = new Station("Death Star");
+		Console.WriteLine("Hello world!");
+        leia.FullName = "Leia Solo";
+        falcon.EnterShip(leia, 1);
+        Console.WriteLine(falcon.Passengers);
+	}
+}
+
+class Person
+{
+	private string firstName;
+	private string lastName;
+	private string alliance;
+	public Person(string firstName, string lastName, string alliance)
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.alliance = alliance;
+	}
+
+	public string FullName
+	{
+		get
+		{
+			return this.firstName + " " + this.lastName;
+		}
+
+		set
+		{
+			string[] names = value.Split(' ');
+			this.firstName = names[0];
+			this.lastName = names[1];
+		}
+	}
+}
+
+class Ship
+{
+	private Person[] passengers;
+	public Ship(string alliance, string type, int size)
+	{
+		this.Type = type;
+		this.Alliance = alliance;
+		this.passengers = new Person[size];
+	}
+
+	public string Type
+	{
+		get;
+		set;
+	}
+
+	public string Alliance
+	{
+		get;
+		set;
+	}
+
+	public string Passengers
+	{
+		get
+		{
+			foreach (var person in passengers)
+			{
+				Console.WriteLine(String.Format("{0}", person.FullName));
+			}
+
+			return "That's Everybody!";
+		}
+	}
+
+	public void EnterShip(Person person, int seat)
+	{
+		this.passengers[seat] = person;
+	}
+
+	public void ExitShip(int seat)
+	{
+		this.passengers[seat] = null;
+	}
+}
