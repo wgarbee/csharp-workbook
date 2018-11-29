29844ba2fbfb7b81a15c86396e5350c1570949be
Wes Garbee <wgarbee@gmail.com>
2018-11-28 19:38:09 -0600

    w8p12 commit

---
+++ week8-practice12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace week8_practice12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[20];
+            Random randomGenerator = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = randomGenerator.Next(0, 100000);
+            }
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
