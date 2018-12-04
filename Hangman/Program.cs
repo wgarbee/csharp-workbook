4dcf89e396cf39594ff63fe08724fa8a88cbe34e
Wes Garbee <wgarbee@gmail.com>
2018-12-03 20:27:35 -0600

    adding Hangman file

---
+++ Hangman/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.StartGame();
+        }
+    }
+
+    class Game
+    {
+        String[] words;
+
+        public Game()
+        {
+
+        }
+
+        public void StartGame()
+        {
+            String file = @"words_alpha.txt";
+            words = File.ReadAllLines(file);
+
+            int numberOfWords = words.Length;
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+        }
+    }
+
+    class Word
+    {
+
+    }
+}
